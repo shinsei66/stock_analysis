dd9099439a948778e5e7f77e393394455ebc35bd
shinsei66 <48095788+shinsei66@users.noreply.github.com>
2020-09-07 03:21:24 +0900

    Revert "[update] fixed output inconvinience"
    This reverts commit 4b6fb350851ac81d2e2cb3b2338b36e04af087e0.

--- config.py
+++ config.py
@@ -14,11 +14,10 @@ stock_price = 'stock_price.csv' #Gathered stock price data
 
 # parameters
 startdate = '2018-10-15' #The minimum date within the trade history data
-enddate = '2020-08-31' #The maximum date within the trade history data
-datafix_enddate = '2020-07-28'
-asset_history = 'asset_history'
+enddate = '2020-07-28' #The maximum date within the trade history data
+asset_history = 'asset_history.csv'
 ifexdata = True #In case we have external stock data, set True. we need customize this part by ourselves
-partition = '202008'
+partition = '202007'
 
 executionstart = time.time()
 
